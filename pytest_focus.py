44d3188d3e6083e91e4a3215ed71b9bf4aa8ef76
Joshua Yee <yeej2@allegheny.edu>
2019-04-09 16:12:27 -0400

    refactor pytest_focus to test plugin
    Co-authored by:
    Matt Baldeosingh <baldeosinghm@allegheny.edu>

--- pytest_focus.py
+++ pytest_focus.py
@@ -3,19 +3,33 @@
 import pytest
 
 
+# def pytest_addoption(parser):
+#     group = parser.getgroup('pytest.focus')
+#     group.addoption(
+#         '--focus',
+#         action='store_true',
+#         dest='/scripts',
+#         default='ptfocus',
+#         help='focus: type --focus after pytest.'
+#     )
+#
+#     parser.addini('/test/conftest.py')
+#
+#
+# #@pytest.fixture
+# def pytest_script():
+#     if pytest.config.getoption('focus'):
+#         return "You are going to be focused."
+#     #return request.config.option.dest_script
+
 def pytest_addoption(parser):
     group = parser.getgroup('focus')
     group.addoption(
         '--focus',
         action='store_true',
-        dest='/scripts',
-        default='ptfocus',
         help='focus: type --focus after pytest.'
     )
 
-    parser.addini('/test/conftest.py')
-
-
-@pytest.fixture
-def script(request):
-    return request.config.option.dest_script
+def pytest_test():
+    if pytest.config.getoption('focus'):
+        return "BLAAAAH"
