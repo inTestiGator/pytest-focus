db34be21c7b1ca80b84e918cd3e6330144f1cee0
Joshua Yee <yeej2@allegheny.edu>
2019-04-09 16:15:02 -0400

    implement, path file so pytest will run "Vikes21"
    Co-authored by:
    Matt Baldeosingh <baldeosinghm@allegheny.edu>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
 pytest_plugins = 'pytester'
+MYPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MYPATH + "/../")
+# pytest_plugins = 'pytester'
+
+def pytest_addoption(parser):
+    group = parser.getgroup('focus')
+    group.addoption(
+        '--focus',
+        action='store_true',
+        help='focus: type --focus after pytest.'
+    )
