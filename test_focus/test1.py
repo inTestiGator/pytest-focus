3b51b25f95ab5d2329589d859fc24a45b1e5f69a
Joshua Yee <yeej2@allegheny.edu>
2019-04-24 14:20:40 -0400

    Add docstrings to test1

--- test_focus/test1.py
+++ test_focus/test1.py
@@ -1,13 +1,22 @@
 def iequals1():
+    """
+    test case for testing plugin
+    """
     i = 1
     return i
 
 
 def iequals2():
+    """
+    test case for testing plugin
+    """
     i = 2
     return i
 
 
 def fail_test():
+    """
+    test case for testing plugin
+    """
     i = 0
     return i
