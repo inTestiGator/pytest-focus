5846f0f277e3376236e6431007e4e128efe6aeef
Joshua Yee <yeej2@allegheny.edu>
2019-04-24 14:20:50 -0400

    Delete unnecessary comments

--- src/pytest_focus.py
+++ src/pytest_focus.py
@@ -5,25 +5,6 @@ Plug in for pytest that sends push notifications for failed tests
 import subprocess
 import pytest
 
-# def pytest_addoption(parser):
-#     group = parser.getgroup('pytest.focus')
-#     group.addoption(
-#         '--focus',
-#         action='store_true',
-#         dest='/scripts',
-#         default='ptfocus',
-#         help='focus: type --focus after pytest.'
-#     )
-#
-#     parser.addini('/test/conftest.py')
-#
-#
-# #@pytest.fixture
-# def pytest_script():
-#     if pytest.config.getoption('focus'):
-#         return "You are going to be focused."
-#     #return request.config.option.dest_script
-
 
 def pytest_addoption(parser):
     """
@@ -47,7 +28,7 @@ def pytest_test():
     if pytest.config.getoption("focus"):
         from sys import platform
 
-        if platform == "linux" or platform == "linux2":
+        if platform #== "linux" or platform == "linux2":
             return "WOOOOOO!"  # linux
             subprocess.call("scripts/linux.sh", shell=True)
         elif platform == "darwin":
