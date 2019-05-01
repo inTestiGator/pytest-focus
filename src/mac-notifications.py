927c5d7b73031c54b7d0f4cff4da3836e86b47ce
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2019-05-01 18:18:27 -0400

    Removed unecessary methods and created main to call notify function

--- src/mac-notifications.py
+++ src/mac-notifications.py
@@ -10,17 +10,5 @@ def notify(title, subtitle, message):
     m = '-message {!r}'.format(message)
     os.system('terminal-notifier {}'.format(' '.join([m, t, s])))
 
-pync.remove_notifications(os.getpid())
-
-pync.list_notifications(os.getpid())
-
-Notifier.notify('Hello World')
-Notifier.notify('Hello World', title='Python')
-Notifier.notify('Hello World', group=os.getpid())
-Notifier.notify('Hello World', activate='com.apple.Safari')
-Notifier.notify('Hello World', open='http://github.com/')
-Notifier.notify('Hello World', execute='say "OMG"')
-
-Notifier.remove(os.getpid())
-
-Notifier.list(os.getpid())
+if __name__== "__main__":
+    notify("Error Notification", "python error", "Check your terminal. You have 3 errors!")
