5914dee391f5fac9811bcf313b10d112c848fad2
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2019-05-03 16:50:04 -0400

    Add module that allows for mac notifications to made

---
+++ src/mac-notifications.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+# The notifier function
+def notify(title, subtitle, message):
+    t = '-title {!r}'.format(title)
+    s = '-subtitle {!r}'.format(subtitle)
+    m = '-message {!r}'.format(message)
+    os.system('terminal-notifier {} -activate "com.apple.Terminal"'.format(' '.join([m, t, s])))
+
+if __name__== "__main__":
+    notify("Failed Test Cases", "uh oh!", "Check your terminal. You have 3 failed test cases!")
+    sys.stdout.write("It appears you have failing test cases...\n")
