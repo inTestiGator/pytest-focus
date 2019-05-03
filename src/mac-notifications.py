16f01cfd9caa15f027ae18604c24e2cac4502d3d
Matthew Baldeosingh <baldeosinghm@allegheny.edu>
2019-05-03 16:54:16 -0400

    Reformat file with black

--- src/mac-notifications.py
+++ src/mac-notifications.py
@@ -4,11 +4,20 @@ import sys
 
 # The notifier function
 def notify(title, subtitle, message):
-    t = '-title {!r}'.format(title)
-    s = '-subtitle {!r}'.format(subtitle)
-    m = '-message {!r}'.format(message)
-    os.system('terminal-notifier {} -activate "com.apple.Terminal"'.format(' '.join([m, t, s])))
+    t = "-title {!r}".format(title)
+    s = "-subtitle {!r}".format(subtitle)
+    m = "-message {!r}".format(message)
+    os.system(
+        'terminal-notifier {} -activate "com.apple.Terminal"'.format(
+            " ".join([m, t, s])
+        )
+    )
 
-if __name__== "__main__":
-    notify("Failed Test Cases", "uh oh!", "Check your terminal. You have 3 failed test cases!")
+
+if __name__ == "__main__":
+    notify(
+        "Failed Test Cases",
+        "uh oh!",
+        "Check your terminal. You have 3 failed test cases!",
+    )
     sys.stdout.write("It appears you have failing test cases...\n")
