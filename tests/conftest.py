f31533c0318100e92bfeca4bded747a1b84e83a1
Mohammad Khan <mohammadnk1@gmail.com>
2019-05-04 19:33:06 -0400

    Revert conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,8 @@ GO_INTO_SRC_DIRECTORY = "src"
 # pytest_plugins = 'pytester'
 
 # set the system path to contain the previous directory
-PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
+PRE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, PRE_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_SRC_DIRECTORY)
 
 
 def mac_notify(title, subtitle, message):
