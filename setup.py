b072282210af20f3e94d8e56516dc1d904406d25
Zac Shaffer <shafferz@allegheny.edu>
2019-05-04 18:24:24 -0400

    Fixed linting issues for TravisCI
    Co-authored-by: baldeosinghm <baldeosinghm@allegheny.edu>

--- setup.py
+++ setup.py
@@ -2,20 +2,18 @@
 # -*- coding: utf-8 -*-
 """ Setup for pytest-focus plugin. """
 
-import os
-import codecs
 from setuptools import setup
 
 setup(
-    name='pytest-focus',
-    version='0.1.1',
-    description='A pytest plugin that alerts user of failed test cases with screen notifications',
-    url='https://github.com/inTestiGator/pytest-focus',
-    author='Matthew Baldeosingh, et al.',
-    author_email='baldeosinghm@allegheny.edu',
-    license='MIT',
+    name="pytest-focus",
+    version="0.1.1",
+    description="A plugin alerting users of failed test cases with notifications",
+    url="https://github.com/inTestiGator/pytest-focus",
+    author="Matthew Baldeosingh, et al.",
+    author_email="baldeosinghm@allegheny.edu",
+    license="MIT",
     # long_description=read("README.md"),
-    py_modules=['pytest_focus'],
+    py_modules=["pytest_focus"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     # install_requires=["pytest>=3.5.0"],
     classifiers=[
@@ -36,6 +34,6 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
-    install_requires=['pytest'],
-    entry_points={'pytest11': ['focus = pytest_focus', ], },
+    install_requires=["pytest"],
+    entry_points={"pytest11": ["focus = pytest_focus"]},
 )
